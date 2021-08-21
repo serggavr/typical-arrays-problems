135e5bc2ed619b2de87789283973361be6348d24
GSergey <example@gmail.com>
2021-08-21 22:33:11 +0300

    fix: 0

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 
 exports.min = function min (array) {
-  if (array.length == 0) return 0;
+  if (array.length < 1) return 0;
   return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  if (array.length == 0) return 0;
+  if (array.length < 1) return 0;
   return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  if (array.length == 0) return 0;
+  if (array.length < 1) return 0;
   let x = 0;
   for (let i = 0; i < array.length; i++) {
     x += array[i]
