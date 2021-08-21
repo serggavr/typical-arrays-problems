2b97a7ac88cc571609faa66daa5de1549951874a
GSergey <example@gmail.com>
2021-08-21 22:31:37 +0300

    fix: avr+

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 
 exports.min = function min (array) {
+  if (array.length == 0) return 0;
   return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
+  if (array.length == 0) return 0;
   return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  let x;
+  if (array.length == 0) return 0;
+  let x = 0;
   for (let i = 0; i < array.length; i++) {
     x += array[i]
   }
