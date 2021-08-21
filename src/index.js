1f614a9ed3ef34922d02cc6769b6bc088c609439
GSergey <example@gmail.com>
2021-08-21 22:19:20 +0300

    init: Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return Math.mean.apply(null, array);
 }
